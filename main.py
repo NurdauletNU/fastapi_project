fbbfcd7470480fc6aa15d5ceb87f853adf6a5ff1
ndlt <angelrangel2210@gmail.com>
2025-10-23 11:26:22 +0500

    Creating API

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -7,3 +10,21 @@ async def read_root():
     return {'message': 'Hello WOrld!'}
 
 
+@app.get('/greet/{name}')
+async def say_hello(name:Optional[str] = 'User', age:int=0)-> dict:
+    return {'message': f'Hello {name}', 'age': age}
+
+
+class BookCreateModel(BaseModel):
+    title: str
+    author: str
+
+
+
+
+@app.post('/create_book')
+async def create_book(book_data: BookCreateModel):
+    return {
+        'title': book_data.title,
+        'author': book_data.author
+    }
