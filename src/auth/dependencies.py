f4edc3a2c6eb5c154c1d5b92ceba28a4e1a3da91
Nurdaulet Uash <angelrangel2210@gmail.com>
2025-10-31 00:53:40 +0600

    JWT Authentication Revoke Access Tokens Using Redis

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -3,7 +3,7 @@ from fastapi import Request, status
 from fastapi.security.http import HTTPAuthorizationCredentials
 from .utils import decode_token
 from fastapi.exceptions import HTTPException
-
+from src.db.redis import token_in_blocklist
 
 
 
@@ -21,7 +21,12 @@ class TokenBearer(HTTPBearer):
         token_data = decode_token(token)
 
         if not self.token_valid:
-            raise HTTPException(status_code = status.HTTP_403_FORBIDDEN, detail = "Invalid or expired token")
+            raise HTTPException(status_code = status.HTTP_403_FORBIDDEN, detail = {"error": "This token is invalod or expired",
+                            "resolution": "Please get new token"})
+        
+        if await token_in_blocklist(token_data['jti']):
+            raise HTTPException(status_code = status.HTTP_403_FORBIDDEN, detail = {"error": "This token is invalod or has been revoked",
+                            "resolution": "Please get new token"})
         
         self.verify_token_data(token_data)
         
@@ -33,7 +38,7 @@ class TokenBearer(HTTPBearer):
 
         token_data = decode_token(token)
         
-        return True if token_data is not None else False
+        return token_data is not None 
     
 
     def verify_token_data(self, token_data):
