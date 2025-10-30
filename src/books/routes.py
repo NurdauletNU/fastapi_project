95e894eff31d6348f0fa601951d59e19f5daf9a5
Nurdaulet Uash <angelrangel2210@gmail.com>
2025-10-30 17:43:58 +0600

    JWT Authentication Protect Endpoints with HTTP Bearer Auth

--- src/books/routes.py
+++ src/books/routes.py
@@ -5,23 +5,23 @@ from fastapi import APIRouter, status, Depends
 from src.db.main import get_session
 from sqlmodel.ext.asyncio.session import AsyncSession
 from src.books.service import BookService
-
+from src.auth.dependencies import AccessTokenBearer
 
 
 book_router = APIRouter()
 book_service = BookService()
-
+access_token_bearer = AccessTokenBearer()
 
 
 
 
 @book_router.get('/', response_model=List[Book])
-async def get_all_books(session: AsyncSession = Depends(get_session)) -> List[Book]:
+async def get_all_books(session: AsyncSession = Depends(get_session), user_details = Depends(access_token_bearer)) -> List[Book]:
     books = await book_service.get_all_books(session)
     return books
 
 @book_router.post('/', status_code=status.HTTP_201_CREATED, response_model=Book)
-async def create_a_book(book_data:BookCreateModel, session: AsyncSession = Depends(get_session))->Book:
+async def create_a_book(book_data:BookCreateModel, session: AsyncSession = Depends(get_session), user_details = Depends(access_token_bearer))->Book:
     new_book = await book_service.create_book(book_data, session)
     return new_book
 
@@ -32,7 +32,7 @@ async def create_a_book(book_data:BookCreateModel, session: AsyncSession = Depen
 
 
 @book_router.get('/{book_uid}', response_model = Book)
-async def get_book(book_uid:str, session: AsyncSession = Depends(get_session))->dict:
+async def get_book(book_uid:str, session: AsyncSession = Depends(get_session), user_details = Depends(access_token_bearer))->dict:
     book = await book_service.get_book(book_uid, session)
     if book:
         return book
@@ -45,7 +45,7 @@ async def get_book(book_uid:str, session: AsyncSession = Depends(get_session))->
 
 
 @book_router.patch('/{book_uid}', response_model=Book)
-async def update_book(book_uid:str, book_update_data:BookUpdateModel, session: AsyncSession = Depends(get_session))->dict:
+async def update_book(book_uid:str, book_update_data:BookUpdateModel, session: AsyncSession = Depends(get_session), user_details = Depends(access_token_bearer))->dict:
 
     updated_book = await book_service.update_book(book_uid, book_update_data, session)
 
@@ -62,7 +62,7 @@ async def update_book(book_uid:str, book_update_data:BookUpdateModel, session: A
 
 
 @book_router.delete('/{book_uid}', status_code=status.HTTP_204_NO_CONTENT)
-async def delete_book(book_uid:str, session: AsyncSession = Depends(get_session)):
+async def delete_book(book_uid:str, session: AsyncSession = Depends(get_session), user_details = Depends(access_token_bearer)):
     deleted_book = await book_service.delete_book(book_uid, session)
     if deleted_book is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Book not found')
